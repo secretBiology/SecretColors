4dc0f628b42fd67ea7e71eaa370b74ab60fa3191
riot <ri0t@users.noreply.github.com>
2020-09-08 21:47:51 +0200

    depends on numpy
    It was listed in requirements.txt but  not setup.py

--- setup.py
+++ setup.py
@@ -26,4 +26,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "numpy==1.19.1"
+    ],
 )
